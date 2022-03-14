189ab8966489f096bdefd7379ac4fe9939d13691
Hugo Rafael <hugorafell@gmail.com>
2022-03-14 20:31:54 -0300

    Update aula 12/03/22

---
+++ Java samples/Aula01.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Programa{
+  public static void main(String[] args){
+
+    // abro um Scanner de entrada pro sistema (Terminal)
+    Scanner in = new Scanner(System.in);
+
+    String nome = in.nextLine();
+    long telefone = in.nextLong();
+    String serie = in.nextLine();
+    double nota1 = 8.0;
+    double nota2 = 6.0;
+    double media = ((nota1 + nota2)/2.0);
+    
+    System.out.println("---------------------");
+    System.out.println("Nome: " + nome);
+    System.out.println("Telefone: " + telefone);
+    System.out.println("Série: " + serie);
+    System.out.println("---------------------");
+    System.out.println("Média = " + media);
+    System.out.println("---------------------");    
+
+    // fecho meu Scanner
+    in.close();
+  }
+
+}
